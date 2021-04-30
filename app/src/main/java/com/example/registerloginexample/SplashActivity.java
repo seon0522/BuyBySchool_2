009e2e4f5b8827ffa396a3f09fe0c9c2ca34822d
KRHDY <1901322@g.yju.ac.kr>
2021-04-30 16:49:25 +0900

    Add project sources

---
+++ app/src/main/java/com/example/registerloginexample/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.example.registerloginexample;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceStare) {
+        super.onCreate(savedInstanceStare);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
+
+
+
