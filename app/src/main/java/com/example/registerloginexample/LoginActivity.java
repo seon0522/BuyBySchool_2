c8f924129a4807e8cd1b77d2cb3841cb2250f0ec
KRHDY <1901322@g.yju.ac.kr>
2021-05-09 14:28:29 +0900

    2021년 5월 9일

--- app/src/main/java/com/example/registerloginexample/LoginActivity.java
+++ app/src/main/java/com/example/registerloginexample/LoginActivity.java
@@ -60,6 +60,7 @@ public class LoginActivity extends AppCompatActivity {
                                 String userPass = jsonObject.getString("userPassword");
 
                                 Toast.makeText(getApplicationContext(),"로그인에 성공하였습니다.",Toast.LENGTH_SHORT).show();
+//                                Intent intent = new Intent(LoginActivity.this, MainListActivity.class);
                                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                                 intent.putExtra("userID", userID);
                                 intent.putExtra("userPass", userPass);
