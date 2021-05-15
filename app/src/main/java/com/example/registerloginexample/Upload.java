f2262906ffea1e56202e6e7c9e98750964f0d5cd
newstar0207 <newstar0207@naver.com>
2021-05-16 00:22:53 +0900

    upload 파일 등록, 취소 버튼시 Intent 추가

--- app/src/main/java/com/example/registerloginexample/Upload.java
+++ app/src/main/java/com/example/registerloginexample/Upload.java
@@ -1,5 +1,6 @@
 package com.example.registerloginexample;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +22,8 @@ public class Upload extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "등록을 취소합니다", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), MainListActivity.class);
+                startActivity(intent);
             }
         });
         uploadBtn = (Button) findViewById(R.id.upload_btn);
@@ -29,6 +32,8 @@ public class Upload extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "게시물을 등록합니다", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), MainListActivity.class);
+                startActivity(intent);
             }
         });
     }
