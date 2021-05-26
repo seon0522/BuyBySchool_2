f2bc34c3c1d52edff3d3a525fcb89d8d7fbd0d00
KRHDY <1901322@g.yju.ac.kr>
2021-05-26 21:40:50 +0900

    2021/05/26

--- app/src/main/java/com/example/registerloginexample/RegisterActivity.java
+++ app/src/main/java/com/example/registerloginexample/RegisterActivity.java
@@ -87,4 +87,3 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 }
-
--- app/src/main/java/com/example/registerloginexample/RegisterRequest.java
+++ app/src/main/java/com/example/registerloginexample/RegisterRequest.java
@@ -32,4 +32,3 @@ public class RegisterRequest extends StringRequest {
         return map;
     }
 }
-
