cc544271ace64772b05a67647b10be82d7e5f5c9
newstar0207 <newstar0207@naver.com>
2021-05-26 19:15:42 +0900

    post 디비에 값 올라감

--- app/src/main/java/com/example/registerloginexample/Upload.java
+++ app/src/main/java/com/example/registerloginexample/Upload.java
@@ -4,22 +4,41 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Upload extends AppCompatActivity {
     Button uploadBtn;
     Button cancelBtn;
 
+    EditText bookText;
+    EditText authorText;
+    EditText priceText;
+    EditText memo;
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+
+        bookText = (EditText) findViewById(R.id.bookText);
+        authorText = (EditText) findViewById(R.id.authorText);
+        priceText = (EditText) findViewById(R.id.priceText);
+        memo = (EditText) findViewById(R.id.memo);
 
         cancelBtn = (Button) findViewById(R.id.cancelUpload_btn);
         cancelBtn.setOnClickListener(new View.OnClickListener() {
@@ -31,15 +50,50 @@ public class Upload extends AppCompatActivity {
             }
         });
         uploadBtn = (Button) findViewById(R.id.upload_btn);
-        uploadBtn.setOnClickListener(new View.OnClickListener(){
+
+//        업로드 버튼 -----------------------------------------------------
+
+        uploadBtn.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "게시물을 등록합니다", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(getApplicationContext(), MainListActivity.class);
-                startActivity(intent);
+
+                String bookName = bookText.getText().toString();
+                String authorName = authorText.getText().toString();
+                int priceSetting = Integer.parseInt(priceText.getText().toString());
+                String detailMemo = memo.getText().toString();
+                String DATA = "2021-05-21";
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String response) {
+                        try{
+                            JSONObject jsonObject = new JSONObject(response);
+                            System.out.println("jsnobject입니다@@@@@@@@@@@@@@@@" + jsonObject);
+                            boolean success = jsonObject.getBoolean("success");
+//                            if(success) {
+                                System.out.println("게시물등록" + response);
+                                Toast.makeText(getApplicationContext(), "게시물을 등록합니다", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(), MainListActivity.class);
+                                startActivity(intent);
+//                            }
+//                            else{
+//                                Toast.makeText(getApplicationContext(),"게시물 등록에 실패하였습니다.",Toast.LENGTH_SHORT).show();
+//                                return;
+//                            }
+
+                        }catch (JSONException e ){
+                            e.printStackTrace();
+                        }
+
+                    }
+                };
+
+                UploadRequest uploadRequest = new UploadRequest(bookName, authorName, detailMemo, priceSetting, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(Upload.this);
+                queue.add(uploadRequest);
             }
         });
     }
-
 }
---
+++ app/src/main/java/com/example/registerloginexample/UploadRequest.java
@@ -0,0 +1,33 @@
+package com.example.registerloginexample;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UploadRequest extends StringRequest {
+
+    final static private String URL = "http://meanzoo.dothome.co.kr/SavePost_Test.php";
+    // 여기 고침
+    private Map<String, String> map;
+
+
+    public UploadRequest(String bookName, String authorName, String detailMemo, int priceSetting, Response.Listener<String> listener) {
+        super(Method.POST, URL, listener, null);
+
+        map = new HashMap<>();
+        map.put("bookName",bookName);
+        map.put("authorName", authorName);
+        map.put("detailMemo",detailMemo);
+        map.put("priceSetting",priceSetting + "");
+        map.put("DATE","2021-02-02");
+
+    }
+
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError {
+        return map;
+    }
+}
